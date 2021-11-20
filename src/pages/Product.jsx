0cc5f2940747c5cc592e944244310848ef275010
nekodu <kaplangokce@yandex.com>
2021-11-20 18:26:57 +0300

    Single Product Page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,170 @@
+import styled from 'styled-components'
+import Navbar from "../components/Navbar"
+import Products from "../components/Products"
+import Newsletter from "../components/Newsletter"
+import Footer from "../components/Footer"
+import  Announcement from "../components/Announcements"
+import { Add, Remove } from '@mui/icons-material'
+
+
+
+const Container = styled.div``
+
+const Wrapper = styled.div`
+padding: 50px;
+display: flex;
+`
+
+const ImgContainer = styled.div`
+flex: 1;
+
+`
+
+const Image = styled.img`
+width: 100%;
+height: 90vh;
+object-fit: cover;
+`
+
+const InfoContainer = styled.div`
+flex: 1;
+padding: 0px 50px;
+`
+
+const Title = styled.h1`
+    font-weight: 200;
+`
+
+const Desc = styled.p`
+    margin: 20px 0px;
+` 
+
+const Price = styled.span`
+    font-weight: 100;
+    font-size: 40px;
+` 
+
+const FilterContainer = styled.div`
+    width: 50%;
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+`
+
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
+`
+
+const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+    margin: 0 5px;
+    cursor: pointer;
+`
+
+const FilterSize = styled.select`
+margin-left: 10px;
+padding: 5px;
+`
+
+const FilterSizeOption = styled.option``
+
+const AddContainer = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+    
+`
+
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border: 2px solid teal;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`
+
+const Button = styled.button`
+    padding: 15px;
+    border:2px solid teal;
+    background-color: white;
+    cursor: pointer;
+    font-weight: 500;
+
+    &:hover{
+        background-color: #f8f4f4 ;
+    }
+`
+
+
+
+const Product = () => {
+    return (
+        <Container>
+            <Navbar/>
+            <Announcement/>
+            <Wrapper>
+                <ImgContainer>
+                    <Image src="https://i.ibb.co/S6qMxwr/jean.jpg"/>
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>Denim Jumpsuit</Title>
+                    <Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla necessitatibus molestiae debitis, fugiat magni ipsa quibusdam aut numquam ad quaerat saepe quas. Recusandae, reprehenderit similique odio ut quia nesciunt sint? Officia harum explicabo voluptatum quae. Corrupti eos alias voluptatibus ad.</Desc>
+                    <Price>$ 20</Price>
+                    <FilterContainer>
+                        <Filter>
+                            <FilterTitle>Color</FilterTitle>
+                            <FilterColor color="black"/>
+                            <FilterColor color="darkblue"/>
+                            <FilterColor color="gray"/>
+                        </Filter>
+                        <Filter>
+                            <FilterTitle>
+                                Size
+                            </FilterTitle>
+                            <FilterSize>
+                            <FilterSizeOption>XS</FilterSizeOption>
+                            <FilterSizeOption>S</FilterSizeOption>
+                            <FilterSizeOption>M</FilterSizeOption>
+                            <FilterSizeOption>XL</FilterSizeOption>
+                            <FilterSizeOption>XXL</FilterSizeOption>
+                            </FilterSize>
+                        </Filter>
+                    </FilterContainer>
+                    <FilterContainer>
+                        <AddContainer>
+                            <AmountContainer>
+                                <Remove/>
+                                <Amount>1</Amount>
+                                <Add/>
+                            </AmountContainer>
+                            <Button>ADD TO CART</Button>
+                        </AddContainer>
+                    </FilterContainer>
+                </InfoContainer>
+            </Wrapper>
+            <Newsletter/>
+            <Footer/>
+        </Container>
+    )
+}
+
+export default Product
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -19,9 +19,13 @@ margin: 20px;
 const FilterText=styled.span`
 font-size: 20px;
 font-weight: 600;
+margin-right: 20px;
 `
 
-const Select=styled.select``
+const Select=styled.select`
+padding: 10px;
+margin-right: 20px;
+`
 const Option=styled.option``
 
 const ProductList = () => {
@@ -35,11 +39,37 @@ const ProductList = () => {
                     <FilterText>
                         Filter Products:
                     </FilterText>
+                    <Select>
+                        <Option disabled selected>Color</Option>
+                        <Option>White</Option>
+                        <Option>Black</Option>
+                        <Option>Red</Option>
+                        <Option>Blue</Option>
+                        <Option>Yellow</Option>
+                        <Option>Green</Option>
+                    </Select>
+                    <Select>
+                        <Option disabled selected>Size</Option>
+                        <Option>XS</Option>
+                        <Option>S</Option>
+                        <Option>M</Option>
+                        <Option>L</Option>
+                        <Option>XL</Option>
+                        <Option>XXL</Option>
+                    </Select>
+
                 </Filter>
                 <Filter>
                     <FilterText>
                         Sort Products:
                     </FilterText>
+                    <Select>
+                        <Option selected>
+                            Newest
+                        </Option>
+                        <Option>Price(asc)</Option>
+                        <Option>Pricde(desc)</Option>
+                    </Select>
                 </Filter>
             </FilterContainer>
             <Products/>
